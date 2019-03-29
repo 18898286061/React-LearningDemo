1f4015e83d90e040262baa615856099dd489ebf6
KaiKai <1035759181@qq.com>
2019-03-29 21:33:04 +0800

    生命周期函数的学习

--- Lifecycle/Demo_1.js
+++ Lifecycle/Demo_1.js
@@ -1,17 +1,27 @@
 let app = document.getElementById("app")
 
+
+// create div
 let div = document.createElement('div')
 
-app.appendChild(div)
-
 let state = 0
-
 div.innerHTML = `
   <p>${state}</p>
   <button>+1</button>
+  <button>Die</button>
 `
+// mount div
+app.appendChild(div)
 
 div.querySelector('button').onclick = () => {
   state += 1
+  // update div
   div.querySelector('p').innerText = state
+}
+
+div.querySelectorAll('button')[1].onclick = ()=> {
+  div.querySelector('button').onclick = null
+  div.querySelectorAll('button')[1].onclick = null
+  div.remove();
+  div = null; // destory div
 }
